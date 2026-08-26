69a478c13450cf998b83c87f60441a0ede8e6dad
Ones <ones.romero@gmail.com>
2026-08-26 16:48:54 -0400

    feat: initialize foundational LangChain project with API configuration and chat model invocation script

---
+++ 1.1-fundational-models.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from pprint import pprint
+
+# Cargar variables de archivo .env
+load_dotenv()
+
+# Inicializar e invocar el modelo
+model = init_chat_model(model="gpt-5-nano")
+
+response = model.invoke("What's the capital of the Moon?")
+
+response
+
+print(response.content)
+
+
+pprint(response.response_metadata)
